0d5b199f2b02ab7225e764f50c97dbe84c947567
Craig Daniel <craigdanielk@gmail.com>
2020-08-27 09:43:13 -0700

    adding site compilied

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Becoming a Distributor",
+        "excerpt":"  Please Contact us via email, or social media for further details","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2020/08/22/distributorship-opportunities.html"
+      },{
+        "title": "Contact",
+        "excerpt":"Contact Information   RSA Contact:          sales@championgrip.com      (+27)012-430-5367      (+27)082-295-8005        USA Contact:          craigdanielk@gmail.com      (+1)323-970-0317      ","categories": ["lawn","bowling"],
+        "tags": [],
+        "url": "http://localhost:4000/lawn/bowling/2020/08/22/Contact-Information.html"
+      },{
+        "title": "How Can I Order Champion Grip",
+        "excerpt":"Ordering   Please contact us via email, and we’ll find your nearest distributor!","categories": ["lawn","bowling"],
+        "tags": [],
+        "url": "http://localhost:4000/lawn/bowling/2020/08/22/how-to-order.html"
+      },{
+        "title": "Social Media",
+        "excerpt":"Links   @ChampionGrip  Facebook  Instagram","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2020/08/22/social-media.html"
+      },{
+        "title": "Weather & Champion Grip",
+        "excerpt":"  On an average bowling day, 1-2 applications per day should should maintain grip for each game.  In rainy weather-more often.  In Sunny weather-less often.  Else, as soon as you feel your grip reducing, simply spray and polish.","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2020/08/22/weather-and-championgrip.html"
+      },{
+        "title": "Application",
+        "excerpt":"Application How To:   Make sure to shake your can before and after every application  This will ensure a high quality coating!  Hold can 20-30cm / 8-12” away from your ball:  Spray until a soft coating of polish is applied, and polish your ball as normal.","categories": ["lawn","bowling"],
+        "tags": [],
+        "url": "http://localhost:4000/lawn/bowling/2020/08/22/Application-of-Spray.html"
+      }]
